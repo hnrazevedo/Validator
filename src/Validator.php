6fd4a67247072db7491aa1854ab937599015d0d1
Henri Azevedo <hnr.azevedo@gmail.com>
2020-08-05 19:56:28 -0300

    Refatorar class Validator

--- src/Validator.php
+++ src/Validator.php
@@ -8,57 +8,82 @@ use Exception;
 Class Validator{
     
     private static array $validators = array();
+    private static ?array $data = null;
 
     public static function add(object $model,callable $return): void
     {
         self::$validators[get_class($model)] = $return($Rules = new Rules($model));
-	}
-
-    public static function execute(array $datas): bool
+    }
+    
+    private static function existData()
     {
-		if(!array_key_exists('data', $datas)){
+        if(!array_key_exists('data', self::$data)){
             throw new Exception('Informações cruciais não foram recebidas.');
         }
+    }
 
-		if(gettype($datas['data']) != 'string' || @json_decode($datas['data']) === null){
+    private static function jsonData()
+    {
+        if(json_decode(self::$data['data']) === null){
             throw new Exception('O servidor recebeu as informações no formato esperado.');
         }
-			    
-		if(!array_key_exists('provider',$datas) || is_null($datas['provider'])){
+    }
+
+    private static function hasProvider()
+    {
+        if(!array_key_exists('provider',self::$data)){
             throw new Exception('O servidor não recebeu o ID do formulário.');
         }
-			    
-		if(!array_key_exists('role',$datas) || is_null($datas['role'])){
+    }
+
+    private static function hasRole()
+    {
+        if(!array_key_exists('role',self::$data)){
             throw new Exception('O servidor não conseguiu identificar a finalidade deste formulário.');
         }
+    }
 
-        $role = $datas['role'];
-            
-        $data = (array) json_decode($datas['data']);
-            
-        $file = (VALIDATOR_CONFIG['path'].ucfirst($datas['provider']).'.php');
-
-        if(file_exists($file)){
-            require_once($file);
+    private static function includeValidations()
+    {
+        if( file_exists(VALIDATOR_CONFIG['path'] . ucfirst(self::$data['provider']) . '.php') ){
+            require_once(VALIDATOR_CONFIG['path'] . ucfirst(self::$data['provider']) . '.php');
         }
+    }
 
-        $class = 'HnrAzevedo\\Validator\\'.ucfirst($datas['provider']);
-
-		if(!class_exists($class)){
+    private static function getClass(string $class)
+    {
+        if(!class_exists($class)){
             throw new Exception("Form ID {$class} inválido.");
         }
 
-        $rules = new $class();
-            
-		if(get_class(self::$validators[get_class($rules)]) !== 'HnrAzevedo\Validator\Rules'){
-            throw new Exception('Ocorreu algum erro e o servidor não pode identificar o responsável por validar o formulário submetido.');
-        }
+        return new $class();
+    }
 
-		if(empty(self::$validators[get_class($rules)]->getRules($role))){
+    private static function existRole($rules)
+    {
+        if(empty(self::$validators[get_class($rules)]->getRules(self::$data['role']))){
             throw new Exception('Não existe regras para validar este formulário.');
         }
+    }
 
-		$validators = self::$validators[get_class($rules)]->getRules($role);
+    public static function execute(array $datas): bool
+    {
+        self::$data = $datas;
+
+		self::existData();
+        self::jsonData();
+        self::hasProvider();
+        self::hasRole();
+		            
+        $data = (array) json_decode($datas['data']);
+            
+        self::includeValidations();
+
+        $rules = self::getClass('HnrAzevedo\\Validator\\'.ucfirst(self::$data['provider']));
+
+		self::existRole($rules);
+
+		$validators = self::$validators[get_class($rules)]->getRules($datas['role']);
 
         $tests = 0;
             
@@ -151,7 +176,7 @@ Class Validator{
                                 foreach ($data as $ke => $sub) {
                                     if($ke===$subvalue){
                                         $equals=true;
-                                        if($valuee!==$sub)
+                                        if($valuee !== $sub)
                                             throw new \Exception(ucfirst($key).' está diferente de '.ucfirst($ke),1);
                                     }
                                 }
@@ -165,7 +190,7 @@ Class Validator{
 			}
         }
             
-		if($tests>$testeds){
+		if($tests > $testeds){
             throw new Exception('Alguma informação necessária não pode ser validada.');
         }
 				
@@ -174,48 +199,35 @@ Class Validator{
 
     public static function toJson(array $request): string
     {
-        $file = (VALIDATOR_CONFIG['path'].ucfirst($request['provider']).'.php');
-        $file = __DIR__.'/../../../../'.$file;
+        self::$data['provider'] = $request['provider'];
 
-        if(file_exists($file)){
-            require_once($file);
-        }
+        self::includeValidations();
 
-
-        $class = 'HnrAzevedo\\Validator\\'.ucfirst($request['provider']);
-
-		if(!class_exists($class)){
-            throw new Exception("Form ID {$class} inválido.");
-        }
-
-		$model = new $class();
-
-		if(get_class(self::$validators[get_class($model)]) !== 'HnrAzevedo\Validator\Rules'){
-            throw new Exception('Ocorreu algum erro e o servidor não pode identificar o responsável por validar o formulário submetido.');
-        }
+        $rules = self::getClass('HnrAzevedo\\Validator\\'.ucfirst($request['provider']));
 		
-		if(empty(self::$validators[get_class($model)]->getRules($request['role']))){
-            throw new Exception('Formulário desativado: Não existem regras para validar este formulário.');
-        }
+		self::existRole($rules);
 		
-		$validators = self::$validators[get_class($model)]->getRules($request['role']);
-
         /* For function to validate information in javascript */
         $response = '{';
-		foreach ($validators as $field => $rules) {
-			$response .= $field.':{';
-			foreach(array_reverse($rules) as $rule => $value){
-				$value = (gettype($value)==='string') ? '\''.$value.'\'' : $value;
+
+		foreach ( self::$validators[get_class($rules)]->getRules($request['role'])  as $field => $r) {
+            $response .= $field.':{';
+                
+			foreach(array_reverse($r) as $rule => $value){
+                $value = (gettype($value)==='string') ? '\''.$value.'\'' : $value;
+                
 				if(gettype($value)==='boolean'){
                     $value = ($value) ? 'true' : 'false';
                 }
-			    $value = ($rule=='regex') ? str_replace('\\','\\\\','\''.substr($value,2,strlen($value)-4).'\'') : $value;
+
+                $value = ($rule=='regex') ? str_replace('\\','\\\\','\''.substr($value,2,strlen($value)-4).'\'') : $value;
+                
 				$response .= $rule.':'.$value.',';
-			}
+            }
+            
 			$response .='},';
         }
 
-        $response = substr(str_replace(',}','}',$response),0,-1).'}';
-		return $response;
+		return substr(str_replace(',}','}',$response),0,-1).'}';
 	}
 }
