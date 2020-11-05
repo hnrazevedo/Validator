c3a2596a82c861c12a8f30f06ab69a8fbef44815
Henri Azevedo <hnr.azevedo@gmail.com>
2020-11-05 10:49:54 -0300

    add methods requests in middlware process

--- src/MiddlewareTrait.php
+++ src/MiddlewareTrait.php
@@ -10,14 +10,27 @@ trait MiddlewareTrait{
 
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
-        $data = ($request->getAttribute('validator') !== null && isset($request->getAttribute('validator')['data'])) ? $request->getAttribute('validator')['data'] : $_REQUEST;
-        $namespace = ($request->getAttribute('validator') !== null && isset($request->getAttribute('validator')['namespace'])) ? $request->getAttribute('validator')['namespace'] : '';
-        $lang = ($request->getAttribute('validator') !== null && isset($request->getAttribute('validator')['lang'])) ? $request->getAttribute('validator')['lang'] : 'en';
-        
         return $handler->handle($request->withAttribute('validator', [
-            'valid' => self::lang($lang)->namespace($namespace)->execute($data),
+            'valid' => self::lang($this->requestLang($request))
+                           ->namespace($this->requestNamespace($request))
+                           ->execute($this->requestData($request)),
             'errors' => self::getErrors()
         ]));
+    }
+
+    private function requestData(ServerRequestInterface $request): array
+    {
+        return ($request->getAttribute('validator') !== null && isset($request->getAttribute('validator')['data'])) ? $request->getAttribute('validator')['data'] : $_REQUEST;
+    }
+
+    private function requestNamespace(ServerRequestInterface $request): array
+    {
+        return ($request->getAttribute('validator') !== null && isset($request->getAttribute('validator')['namespace'])) ? $request->getAttribute('validator')['namespace'] : '';
+    }
+
+    private function requestLang(ServerRequestInterface $request): array
+    {
+        return ($request->getAttribute('validator') !== null && isset($request->getAttribute('validator')['lang'])) ? $request->getAttribute('validator')['lang'] : 'en';
     }
     
 }
